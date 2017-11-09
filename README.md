3374296fe2d94510f13eabc30db58e6efe35ad0b
Hiroshi Murakami <piroz@users.noreply.github.com>
2017-11-10 08:22:21 +0900

    simple notify test

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ghome-notifer-ui
 web base ui for google-home-notify
+
+# how to run
+
+```
+$ npm install ghome-notifer-ui
+$ export DEVICE_NAME=your google home name
+$ npm start
+```
+
+and open localhost:3000 from your browser
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const googlehome = require("google-home-notifier");
+
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+const server = app.listen(3000, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+app.get("/", function(req, res, next){
+    res.render("index", {});
+});
+
+app.post("/", function(req, res, next){
+
+    let deviceName = process.env.DEVICE_NAME ? process.env.DEVICE_NAME : '';
+    
+    googlehome.device(deviceName, 'ja');
+    googlehome.notify(req.param('word'), function(res) {
+        console.log(res);
+    });
+    res.render("index", {});
+});
